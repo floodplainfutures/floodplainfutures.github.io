2aa0db9ac3bd3ed497d061857208e93ce371d920
Zahra <47546484+ZahraBaxi@users.noreply.github.com>
2026-03-03 09:24:07 -0800

    map change

--- map/map.js
+++ map/map.js
@@ -25,9 +25,10 @@
     // }).addTo(map);
     
     // Option 2: Stamen Toner (black & white, high contrast)
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://stamen.com/">Stamen Design</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        maxZoom: 20
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> &copy; <a href="https://carto.com">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
     }).addTo(map);
     
     // Option 3: Stamen Toner Lite (softer black & white)
